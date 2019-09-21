a27a3059e318d3a7165d5c3e89100cd671d91d8b
George Mount <gxm204@case.edu>
2019-09-21 16:50:20 -0400

    Update JOIN files

--- 6-from-vlookup-to-join/inner-join-demo.sql
+++ 6-from-vlookup-to-join/inner-join-demo.sql
@@ -5,6 +5,13 @@ FROM AwardsPlayers;
 SELECT * 
 FROM Master;
 
+-- We can explicitly name
+-- which table a field
+-- should come from
+
+SELECT Master.playerID, Master.nameFirst, Master.nameLast
+FROM Master;
+
 -- INNER JOIN
 SELECT *
 FROM Master INNER JOIN AwardsPlayers
@@ -12,6 +19,15 @@ ON Master.playerID = AwardsPlayers.playerID;
 
 -- Check out the new fields!
 
+-- We don't need dupes
+SELECT Master.playerID, nameFirst, nameLast, yearID, awardID
+FROM Master INNER JOIN AwardsPlayers
+ON Master.playerID = AwardsPlayers.playerID;
+
+-- Vic Janowicz, Ohio State legend
+SELECT *
+FROM Master
+WHERE playerID = 'janowvi01';
 
 -- what happened to Vic Janowicz?
 -- janowvi01
